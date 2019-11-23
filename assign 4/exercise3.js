c165bfcc4a8d18fdb8938327757f20da384961b9
saralkbhagat <saralbhagat0@gmail.com>
2019-11-22 23:48:21 -0800

    Commit for assignment 5

---
+++ assign 4/exercise3.js
@@ -0,0 +1,9 @@
+function func1(){
+    console.log("Username: ",this.event.target[0].value);
+    console.log("Email: ",this.event.target[1].value);
+    console.log("Comment: ", this.event.target[2].value);
+}
+    
+
+
+
